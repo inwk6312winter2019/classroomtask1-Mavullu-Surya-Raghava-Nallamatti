b26ff761d03f4166437fc416a83630419c090053
Mavullu-Surya-Raghava-Nallamatti <mv519522@dal.ca>
2019-01-30 12:08:49 -0400

    myclass.py

--- myclass.py
+++ myclass.py
@@ -5,9 +5,16 @@ print(k)
 """
 
 
-class myip():
-	def __init__(self, iplist=[0xff,0xff,0xff,0xff]):
-		self.mlist=iplist
-uip=myip()
-print(uip.mlist)
+class ipaddr():
+	def __init__(self, ip=[0,0,0,0], nm=[0,0,0,0]):
+		self.ip=ip
+		self.nm=nm
+	def printip(self):
+		ipformat=""
+		for ips in self.ip:
+			ipformat=ipformat+str(ips)+"."
+		print('The address is ' +ipformat)
+
+myip=ipaddr([192,168,1,1],[255,255,255,0])
+myip.printip()
 
